6e38e61256cad67c37005abce82b2473d731b19a
Carrosen <carla1.rosen@gmail.com>
2019-05-26 19:02:08 +0200

    Adds button onClick event, modifies App.jsx, adds if statement for changing greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Button, Input } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
-const App = () => {
+const App = (props) => {
   return (
     <>
       <Container>
         <Header
           as='h1'>
-           Hello World
+          {props.state.greeting}
         </Header>
+        <Input 
+          placeholder='Input new greeting'
+          onBlur={(event) => props.dispatch(
+            { type: 'PROPOSE_GREETING', greeting: event.target.value })
+          }
+        />
+        <Button
+          primary
+          onClick={() => props.dispatch(
+            { type: 'CHANGE_GREETING' })}
+        >
+          Change greeting
+        </Button>
       </Container>
     </>
   );
 }
 
-export default connect()(App)
+const mapStateToProps = (state) => {
+  return {
+    state: state
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(App)
--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,7 +1,14 @@
 import initialState from '../store/initialState'
 
-const rootReducer = (state = initialState) => {
+const rootReducer = (state = initialState, action) => {
+  if (action.type === 'CHANGE_GREETING') {
+    return {
+      ...state,
+      greeting: state.proposed_greeting
+    }
+  } else if {
   return state
+  }
 }
 
 export default rootReducer
